42d37346aec42e988e5a7ae6b772db725d99b87b
gitittttf <simon.erb.ddf@gmail.com>
2025-10-06 19:02:41 +0200

    First code

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use bevy::prelude::*;
+
+mod game_state;
+
+// import plugin modules TODO
+// mod combat;
+// mod character;
+// mod world;
+// mod ui;
+
+pub use game_state::GameState;
+
+// Main plugin that ties everything together
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .init_state::<GameState>()
+            //plugin registrations TODO
+            // .add_plugins(combat::plugin)
+            // .add_plugins(character::plugin)
+            // etc
+            ;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use bevy::prelude::*;
+use informatik_game_bevy::GamePlugin;
+
 fn main() {
-    // Entry
+    App::new()
+        .add_plugins(DefaultPlugins) // Window, rendering und input
+        .add_plugins(GamePlugin) // Game logik
+        .run(); // Bevy's game loop
 }

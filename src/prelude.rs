4348416512e1990a2033cd605a138fbbbbc9240c
gitittttf <simon.erb.ddf@gmail.com>
2025-10-07 15:07:31 +0200

    Common imports in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,1 +1,25 @@
-// TODO
+pub use crate::game_state::GameState;
+
+// Character exports
+pub use crate::character::{
+    bundles::*,
+    components::*,
+};
+
+// Combat exports
+pub use crate::combat::{
+    components::*,
+    events::*,
+    resources::*,
+    systems::*,
+};
+
+// World exports
+pub use crate::world::{
+    data::*,
+    resources::*,
+    systems::*,
+};
+
+// Re-export commonly used Bevy types
+pub use bevy::prelude::*;
